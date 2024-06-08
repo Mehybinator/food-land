5e673733d4bbbaf54615099b9cc647b6c5c5e2a2
Mehybinator <mehpoolio715@gmail.com>
2024-06-08 12:14:51 +0330

    Header Component, Direction rtl!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import "../app.css";
     import "@fortawesome/fontawesome-free/css/all.min.css";
+    import Header from "./Header.svelte";
 </script>
 
-<slot />
+<div class="container m-auto">
+    <Header />
+    <slot />
+</div>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+</script>
+
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a href="#" class="btn btn-ghost text-xl text-primary">daisyUI</a>
+  </div>
+  <div class="flex gap-2">
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn">ورود/ثبت نام</div>
+      <ul
+        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <a href="#" class="justify-between">
+            Profile
+            <span class="badge">New</span>
+          </a>
+        </li>
+        <li><a href="#">Settings</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+    </div>
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <div class="indicator">
+          <span class="badge badge-sm indicator-item indicator-start">8</span>
+          <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+      </div>
+      <div
+        class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+      >
+        <div class="card-body">
+          <span class="font-bold text-lg">8 Items</span>
+          <span class="text-info">Subtotal: $999</span>
+          <div class="card-actions">
+            <button class="btn btn-primary btn-block">View cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
