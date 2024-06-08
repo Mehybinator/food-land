fbd09d05b9b6f972e1c490341fc724a79117efa5
Mehybinator <mehpoolio715@gmail.com>
2024-06-08 12:57:11 +0330

    Footer Component!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
     import "../app.css";
     import "@fortawesome/fontawesome-free/css/all.min.css";
     import Header from "./Header.svelte";
+    import Footer from "./Footer.svelte";
 </script>
 
 <div class="container m-auto">
     <Header />
     <slot />
+    <Footer />
 </div>
