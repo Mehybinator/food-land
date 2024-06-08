0856c08284881deae13ce045453a1a7399e53b2b
Mehybinator <mehpoolio715@gmail.com>
2024-06-08 14:33:57 +0330

    Card Added!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,12 @@
     import "@fortawesome/fontawesome-free/css/all.min.css";
     import Header from "./Header.svelte";
     import Footer from "./Footer.svelte";
+    import Card from "./Card.svelte";
 </script>
 
 <div class="container m-auto">
     <Header />
+    <Card />
     <slot />
     <Footer />
 </div>
---
+++ src/routes/Card.svelte
@@ -0,0 +1,16 @@
+<div class="card w-80 h-96 bg-base-200">
+  <figure class="w-52 h-52 self-center">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      alt="Shoes"
+    />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">بیگ مک برگر</h2>
+    <p>100 گرم برگر دستساز، کاهو، گوجه، سیب زمینی</p>
+    <div class="flex flex-row justify-between items-end">
+      <p>320,000 ت</p>
+      <button class="btn btn-primary w-28 h-11">افزودن به سبد</button>
+    </div>
+  </div>
+</div>
