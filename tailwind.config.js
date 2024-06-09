53f568e6956cb4c0eeafde58e3ec94eb4609ba6a
Mehybinator <mehpoolio715@gmail.com>
2024-06-09 11:53:02 +0330

    New Base Theme!

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,30 @@ export default {
   ],
   daisyui: {
     themes: [
-      "bumblebee",
+      {
+        mainTheme: {
+          "primary": "#ea580c",
+          "primary-content": "#f5f5f3",
+          "secondary": "#dc2626",
+          "secondary-content": "#f5f5f4",
+          "accent": "#1f2937",
+          "accent-content": "#cdd0d3",
+          "neutral": "#111827",
+          "neutral-content": "#c9cbcf",
+          "base-100": "#f8ffff",
+          "base-200": "#d8dede",
+          "base-300": "#b8bebe",
+          "base-content": "#151616",
+          "info": "#111827",
+          "info-content": "#c9cbcf",
+          "success": "#22c55e",
+          "success-content": "#000e03",
+          "warning": "#facc15",
+          "warning-content": "#150f00",
+          "error": "#e11d48",
+          "error-content": "#ffd8d9",
+        },
+      },
     ],
   },
 }
