b72569fa8ecf01f3c0dcb9f882bff3889e54372b
Mehybinator <mehpoolio715@gmail.com>
2024-06-09 15:17:30 +0330

    New Special Offers Component!

---
+++ src/routes/SpecialOffers.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import DiscountCard from "./DiscountCard.svelte";
+</script>
+
+<div
+  class="flex flex-row overflow-auto gap-4 py-10 bg-base-200 bg-opacity-50 rounded-2xl"
+>
+  <div class="flex flex-col min-w-36 max-w-36 justify-center items-center pr-4">
+    <h2 class="text-2xl">Special</h2>
+    <p>offers</p>
+  </div>
+  <DiscountCard />
+  <DiscountCard />
+  <DiscountCard />
+  <DiscountCard />
+  <DiscountCard />
+  <DiscountCard />
+  <DiscountCard />
+  <DiscountCard />
+</div>
