63dd3a036f97424e6cae0dbd16f6b7d955c79a0c
Mehybinator <mehpoolio715@gmail.com>
2024-06-09 14:42:50 +0330

    Discount Card Added!

---
+++ src/routes/DiscountCard.svelte
@@ -0,0 +1,22 @@
+<div class="card w-80 h-[28rem] bg-base-100 shadow-xl group">
+  <figure
+    class="transition-transform w-52 h-52 self-center group-hover:scale-125"
+  >
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      alt="Shoes"
+    />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">بیگ مک برگر</h2>
+    <p>100 گرم برگر دستساز، کاهو، گوجه، سیب زمینی</p>
+    <div class="flex flex-row justify-between items-end">
+      <div class="flex items-center gap-2">
+        <p class="line-through opacity-70 text-sm">400,000 ت</p>
+        <p>200,000 ت</p>
+      </div>
+      <div class="badge badge-primary">50%</div>
+    </div>
+    <button class="btn btn-primary w-full h-11">افزودن به سبد</button>
+  </div>
+</div>
