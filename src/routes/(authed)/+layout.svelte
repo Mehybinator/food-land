9c277f386640f08f9dc622d1ff14808135078ee1
Mehybinator <mehpoolio715@gmail.com>
2024-06-11 10:48:08 +0330

    Grouped Routing For Multiple Headers!(probably overkill)

---
+++ src/routes/(authed)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Header from "$lib/components/Header.svelte";
+</script>
+
+<Header />
+<div class="mx-12">
+    <slot />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import "../app.css";
     import "@fortawesome/fontawesome-free/css/all.min.css";
-    import Header from "$lib/components/Header.svelte";
     import Footer from "$lib/components/Footer.svelte";
 </script>
 
-<Header />
-<div class="mx-12">
-    <slot />
-</div>
+<svelte:head>
+    <title>Food Land</title>
+</svelte:head>
+
+<slot />
 <Footer />
